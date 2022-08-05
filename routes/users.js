c23e8269dc9d78f150704e5d9ff8bf0ff7418c67
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-05 19:41:00 +0500

    work complete

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 import express  from "express"
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router=express.Router()
 const users=[
@@ -26,6 +28,8 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
 
     const user= req.body
+
+const userId=    uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
     users.push(user)
    
     res.send(`user with name ${user.firstName} added to the database`)
